ba371621560a0c996bde3494288c463dbf09e995
jSwAggy01 <jdele022@ucr.edu>
2022-12-08 20:54:11 -0800

    Added code to read text file columns

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 using namespace std;
 
 void nearestNeighbor(double);
@@ -10,14 +14,52 @@ struct data {/*
     id = <number>
 */};
 
+/*
+Sources:
+   Reading columns: https://cplusplus.com/forum/beginner/269288/ 
+   Convert string to double: https://www.geeksforgeeks.org/cpp-program-for-string-to-double-conversion/#:~:text=1.-,Using%20stod(),%3A%3Asize_t*%20pos%20%3D%200)%3B
+*/
 int main() {
+   vector<string> classes;
+   vector<string> features;
+   double c1;
+   double c2;
 
-    /* User interface will be here */
+   // Open the file
+   ifstream inFile("Data/CS170_Small_Data__125.txt");
+   
+   // Error if opened incorrectly
+   if (!inFile) {
+      cout << "Can't open file\n";
+      return 1;
+   }
 
-    return 0;
+   // Get the 1st two columns into a string
+   string line, col1, col2;
+   while (getline(inFile, line)) {
+      stringstream(line) >> col1 >> col2;
+      classes.push_back(col1);
+      features.push_back(col2);
+   }
+
+   // Output the columns and convert to double
+   cout << "***CLASSES***\n";
+   for (string cl : classes) {
+      c1 = stod(cl);
+      cout << c1 << '\n';
+   }
+   cout << "***FEATURES***:\n";
+   for (string fts : features) {
+      c2 = stod(fts);
+      cout << c2 << '\n';
+   }
+   return 0;
 }
 
-void nearestNeighbor(double z) {/*
+// Compile with: g++ test.cpp -o test
+
+void nearestNeighbor(double z) {
+    /*
     If closest distance to z is x
         z <- x
     else
